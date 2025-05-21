7c4b3b9c0b3ea7a7792239415cbe8d82da76c156
fayodayo <165217859+fayodayo@users.noreply.github.com>
2025-05-21 14:34:59 +0100

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -5,7 +5,7 @@ import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
-    { path: '/', component: Challenges },
+    { path: '/challenges', component: Challenges },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
